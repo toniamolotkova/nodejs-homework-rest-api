be8c8dcd3c20f6ef4d68f6faafb5ba661e5e86fa
Antonina Molotkova <molotkovaantonina@gmail.com>
2021-11-22 12:42:40 +0200

    main part

---
+++ controllers/users/updateAvatar.js
@@ -0,0 +1,34 @@
+const fs = require('fs/promises')
+const path = require('path')
+const Jimp = require('jimp')
+const { randomUUID } = require('crypto')
+
+const { User } = require('../../models')
+
+const userPath = path.join(__dirname, '../../public/avatars')
+
+const updateAvatar = async (req, res, next) => {
+  const { _id } = req.user
+  console.log(req.file)
+  const file = await Jimp.read(req.file.path)
+  await file.resize(250, 250)
+
+  const { path: tempUpload, originalname } = req.file
+
+  try {
+    const filename = `${randomUUID()}_${originalname}`
+    const resultUpload = path.join(userPath, filename)
+    console.log(resultUpload)
+    await fs.rename(tempUpload, resultUpload)
+    const avatarURL = path.join('/avatars', filename)
+    await User.findByIdAndUpdate(_id, { avatarURL }, { new: true })
+    res.status(200).json({
+      avatarURL: avatarURL,
+    })
+  } catch (error) {
+    fs.unlink(tempUpload)
+    next(error)
+  }
+}
+
+module.exports = updateAvatar
